3cfc9579b6d4749070ffd128ea8147303204599b
tanmoyh <t.hossain@newcastle.ac.uk>
2018-04-23 03:01:53 +0100

    need to fix link, JSON uploaded but room info not working yet - Tanmoy

--- app/src/main/java/com/example/grigo/usb_explore/PinViewFragment.java
+++ app/src/main/java/com/example/grigo/usb_explore/PinViewFragment.java
@@ -1,16 +1,22 @@
 package com.example.grigo.usb_explore;
 
 import android.graphics.PointF;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.GestureDetector;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
@@ -18,8 +24,16 @@ import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 import com.example.grigo.usb_explore.R.layout;
 import com.example.grigo.usb_explore.R.id;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.OptionalInt;
 import java.util.stream.IntStream;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 public class PinViewFragment extends Fragment {
 
@@ -57,12 +71,16 @@ public class PinViewFragment extends Fragment {
 
     Button btnDisabledAccess;
 
+    private String jsonString = "";
+    String roomNo = "";
+    int level = 0;
+
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         rootView = inflater.inflate(layout.pin_view_fragment, container, false);
-
 
 
         mapSetup(); // sets up the map
@@ -77,12 +95,15 @@ public class PinViewFragment extends Fragment {
         });
 
         try {
-            String roomNo = getActivity().getIntent().getStringExtra("ROOM_NUMBER");
-            int level = Integer.parseInt(getActivity().getIntent().getStringExtra("LEVEL"));
-            Log.d("FROG", roomNo + " on " + level);
-            setPinNoIdx(roomNo, level);
+            roomNo = getActivity().getIntent().getStringExtra("ROOM_NUMBER");
+            level = Integer.parseInt(getActivity().getIntent().getStringExtra("LEVEL"));
+            getActivity().getIntent().removeExtra("ROOM_NUMBER");
+            getActivity().getIntent().removeExtra("LEVEL");
+            if (level != 0){
+                setPinNoIdx(roomNo, level);
+            }
+
         } catch (Exception e){
-            Log.d("FROG", e.getMessage());
         }
 
         return rootView;
@@ -106,12 +127,69 @@ public class PinViewFragment extends Fragment {
 
                         setPin(MapActivity.floorList.get(floorNum).getRoomsList().get(i).getRoomName(), i); // calls method that sets the pin at the correct coordinates for the tapped room
                         System.out.println(MapActivity.floorList.get(floorNum).getRoomsList().get(i).getRoomName()); // (debugging) prints out room name
+                        displayInfo(MapActivity.floorList.get(floorNum).getRoomsList().get(i).getRoomName());
                     }
                 }
             }
             return true;
         }
     });
+
+    public void loadJSON() {
+        try {
+            InputStream is = getActivity().getAssets().open("JSONS/roominfo.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            jsonString = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            Log.d("FROG", "JSON LOAD NOT WORKING");
+        }
+    }
+
+
+    public void displayInfo(String roomName){
+        loadJSON();
+        String info = "";
+        try {
+            JSONObject obj = new JSONObject(jsonString);
+            JSONArray rooms = obj.getJSONArray("rooms");
+            for (int x = 0; x < rooms.length(); x++){
+                Log.d("FROG", rooms.getJSONObject(x).getString("Room Name"));
+                if (roomName.equalsIgnoreCase(rooms.getJSONObject(x).getString("Room Name"))){
+                    info = rooms.getJSONObject(x).toString();
+                }
+            }
+        } catch (JSONException e) {
+            Log.d("FROG", e.getMessage());
+        }
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        // Inflate the custom layout/view
+        View popupView = inflater.inflate(layout.room_popup,null);
+        // Initialize a new instance of popup window
+        PopupWindow popup = new PopupWindow(
+                popupView,
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        popup.setElevation(5.0f);
+
+        // Get a reference for the custom view close button
+        ImageButton closeButton = popupView.findViewById(R.id.ib_close);
+        TextView tv = popupView.findViewById(id.popuptv);
+        tv.setText(info);
+        // Set a click listener for the popup window close button
+        closeButton.setOnClickListener(view -> {
+            // Dismiss the popup window
+            popup.dismiss();
+        });
+        popup.showAtLocation(getActivity().findViewById(id.mapView), Gravity.CENTER,0,0);
+    }
+
+
+
 
 
     public void mapSetup() {
