5c046c50043ac4eb1e4556a603616a130f124b99
chrisgrigo <grigoriadischr@gmail.com>
2018-04-23 16:20:52 +0100

    Second Push changes

--- app/src/main/java/com/example/grigo/usb_explore/SettingsActivity.java
+++ app/src/main/java/com/example/grigo/usb_explore/SettingsActivity.java
@@ -166,6 +166,9 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
+            //Examplelist1 - Language
+            //Examplelist2 - ColourBlind
+            //Examplelist3 - Navigation
             bindPreferenceSummaryToValue(findPreference("example_list"));
             bindPreferenceSummaryToValue(findPreference("example_list2"));
             bindPreferenceSummaryToValue(findPreference("example_list3"));
